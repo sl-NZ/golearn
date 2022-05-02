65093e4756a70b70747d93625a76fab836ec18d3
Sanil Lad <sanillad@gmail.com>
2022-05-02 21:41:01 +1200

    updated assert message call and fixed broken code

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -13,18 +13,12 @@ func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
 		got := Hello("Chris")
 		want := "Hello, Chris"
-
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say, 'Hello, World', when an emtpy string is supplied", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, World"
-
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 }
